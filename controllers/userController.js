f25dbb397366a6a699e9f932bb622d2a91403ed9
Slayver <mohamed.a.yehia9@gmail.com>
2025-12-22 07:17:37 +0200

    initial commit: too late for it but here we are :)

---
+++ controllers/userController.js
@@ -0,0 +1,42 @@
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This Route Is Not Yet Defined!',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This Route Is Not Yet Defined!',
+  });
+};
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This Route Is Not Yet Defined!',
+  });
+};
+
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This Route Is Not Yet Defined!',
+  });
+};
+
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This Route Is Not Yet Defined!',
+  });
+};
+
+module.exports = {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
---
+++ routes/tourRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const {
+  createTour,
+  deleteTour,
+  getAllTours,
+  getTour,
+  updateTour,
+} = require('./../controllers/tourController');
+
+const router = express.Router();
+
+// router.param('id', checkId);
+
+router.route('/').get(getAllTours).post(createTour);
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+module.exports = router;
