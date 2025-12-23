f91fa754c59e928804465a566feb0bbf0458c008
Slayver <mohamed.a.yehia9@gmail.com>
2025-12-23 06:30:14 +0200

    impelement: sorting, filtering and making the data sorted by createdAt by default

--- models/tourModel.js
+++ models/tourModel.js
@@ -19,7 +19,7 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a difficulty'],
   },
-  ratingAverage: { type: Number, default: 4.5 },
+  ratingsAverage: { type: Number },
   ratingQuantity: { type: Number, default: 0 },
   price: { type: Number, required: [true, 'A tour must have a price'] },
   priceDiscount: Number,
