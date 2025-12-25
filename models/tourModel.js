e1f0a6abd010378bbbb44e3fb053f707d7ebe329
Slayver <mohamed.a.yehia9@gmail.com>
2025-12-25 07:26:42 +0200

    impelement: document middleware to doc our saved tours and create slugs for them using slugify

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -54,6 +56,19 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// DOCUMENT MIDDLEWARE: Runs Before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+});
+
+// tourSchema.pre('save', function () {
+//   console.log('Will Save Document..');
+// });
+
+// tourSchema.post('save', function (doc) {
+//   console.log(doc);
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
