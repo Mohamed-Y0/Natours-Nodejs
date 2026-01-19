d8ee540af8db8126b83bf2d75a69d230c1177e76
Slayver <mohamed.a.yehia9@gmail.com>
2026-01-19 06:05:47 +0200

    impelement: validation in passwordConfirm & creating a middleware to hash the password before sending it to the database

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, 'Please tell us your name!'] },
@@ -19,7 +20,25 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function () {
+  // Only run this function is password was actually modified
+  if (!this.isModified('password')) return;
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
 });
 
 const User = mongoose.model('User', userSchema);
