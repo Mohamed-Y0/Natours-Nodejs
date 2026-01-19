7ca7f724ec35d06751ffec355d05096aba93a478
Slayver <mohamed.a.yehia9@gmail.com>
2026-01-19 05:36:43 +0200

    impelement: user schema & creating new users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,8 +7,11 @@ const {
   getUser,
   updateUser,
 } = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
