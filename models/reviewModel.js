7cc56b910b16e677ffdcc679751aa43b3ec1f9b8
Slayver <mohamed.a.yehia9@gmail.com>
2026-01-27 02:40:15 +0200

    feat: calculating average rating on tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+import Tour from './tourModel.js';
+
 const reviewSchema = new mongoose.Schema(
   {
     review: { type: String, required: [true, 'Review can not be empty'] },
@@ -30,11 +32,49 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
-reviewSchema.pre(/^find/, function () {
+reviewSchema.post(/^find/, function () {
   this.populate({
     path: 'user',
     select: 'name photo',
   });
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function () {
+  this.r = await this.findOne();
+  console.log(this.r);
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
