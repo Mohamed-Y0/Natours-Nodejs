bc54fb04bb5f2e3d6a09b71d2b9d5173a092443d
Slayver <mohamed.a.yehia9@gmail.com>
2026-02-18 06:53:36 +0200

    feat: creating new booking on checkout success

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,6 +3,7 @@ import Tour from './../models/tourModel.js';
 import catchAsync from './../utils/catchAsync.js';
 import * as factory from './handlerFactory.js';
 import AppError from '../utils/appError.js';
+import Booking from '../models/bookingModel.js';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -12,7 +13,7 @@ export const getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -38,4 +39,14 @@ export const getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+export const createBookingCheckout = catchAsync(async (req, res, next) => {
+  // TEMPORARY
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,10 +6,11 @@ import {
   getTour,
 } from '../controllers/viewController.js';
 import { isLoggedIn, protect } from '../controllers/authController.js';
+import { createBookingCheckout } from '../controllers/bookingController.js';
 
 const router = express.Router();
 
-router.get('/', isLoggedIn, getOverview);
+router.get('/', createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
